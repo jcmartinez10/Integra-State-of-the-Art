baa37f9ec5fbe06aa4aa4b6a100bb2e95c300e0c
Edgar Céspedes Sabogal <37550240+EdgarCsA@users.noreply.github.com>
2020-11-12 12:15:33 -0500

    Pruebas scholarly and MongoBD

--- Librerias consultas_GoogleScholar/Pruebas1.py
+++ Librerias consultas_GoogleScholar/Pruebas1.py
@@ -13,14 +13,30 @@ Autor.fill()
 
 Todo=Autor.fill()
 
+Todo_dic=vars(Todo)
+
+
+
 Publicaciones=Todo.publications
+Prueba=Publicaciones[0]
+d=Prueba.fill().bib
+print(d)
 Pu=[]
-for Publicacion1 in Publicaciones:
-    Pub=Publicacion1.bib
-    Pu.append([Pub["title"],
+
+
+for p in Publicaciones:
+    Pu.append(p.fill().bib)
+
+Pu[1].keys()
+
+
+#### Inviable crear tablas porque en cada pulicación no exiten todos los campos. 
+'''for Publicacion1 in Publicaciones:
+    Pub=Publicacion1.fill().bib
+    print(Pub["title"])
+    LI=[Pub["title"],
     Pub["abstract"],
     Pub["author"],
-    Pub["cites"],
     Pub["cites"],
     Pub["cites_id"],
     Pub["eprint"],
@@ -29,9 +45,26 @@ for Publicacion1 in Publicaciones:
     Pub["pages"],
     Pub["publisher"],
     Pub["url"],
-    Pub["volume"],
-    Pub["year"]])
-    
+    #Pub["volume"],
+    Pub["year"]]
+    Pu.append(LI)'''
+
+####Se propone gestionas una  base de datos NoSQL ####   
+##Almacenamiento inicial en mongo BD#
+
+import pymongo
+
+#Se debe tener previamente corriendo en servidor de MongoDB
+client = pymongo.MongoClient()
+#Como prueba se imprimen la lista de BD que se están gestionando desde Mongo-server
+for bd in client.list_databases(): print(bd)
+
+#Conexion a una DB llamada "Journals1" como prueba inicial para agregar una primera colección
+bd=client["Journals1"]
+mycol = bd["Autores"]
+
+for p in Pu:
+    mycol.insert_one(p)
 
 
 
